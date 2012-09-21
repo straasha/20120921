4932afb341e2971bf6a95d446889e74e54fd732f
小松仁 <komatsu.jin@adways.net>
2012-09-21 22:02:41 +0900

    update 22:02

--- 20120917_1/Test3ViewController.h
+++ 20120917_1/Test3ViewController.h
@@ -7,14 +7,25 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "LoadingView.h"
 
 @interface Test3ViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+{
+  dispatch_queue_t mainQueue;  
+  dispatch_queue_t subQueue;
+  UIActivityIndicatorView *loadingIndicator;
+  LoadingView *loadingView;   
+}
+
 
 @property (retain, nonatomic) IBOutlet UITableView *table;
-
 
 @property( retain, nonatomic )NSMutableArray *arr;
 
 @property (retain, nonatomic) IBOutlet UIButton *BUTTON;
+
+@property( assign, nonatomic ) BOOL flg;
+
 - (IBAction)buttonPush:(id)sender;
+
 @end
--- 20120917_1/Test3ViewController.m
+++ 20120917_1/Test3ViewController.m
@@ -16,9 +16,82 @@
 
 @synthesize table;
 
-@synthesize arr;
+@synthesize arr, flg;
 
 @synthesize BUTTON;
+
+-(void)tableView:(UITableView*)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+
+  if (indexPath.row >= ( arr.count -1) ) {
+  
+ 
+  //画面を表示する
+  [loadingView addSubview:loadingIndicator];
+  [self.view addSubview:loadingView];
+
+  //非同期処理のQueueをセット
+  mainQueue = dispatch_get_main_queue();
+  subQueue = dispatch_queue_create("utaate_sub_queue", NULL);
+    [table beginUpdates];
+
+  //非同期処理
+  dispatch_async(subQueue, ^{
+    [self execSubQueue :indexPath];
+
+    //メインキュー
+    dispatch_async(mainQueue, ^{
+      [self execMainQueue];
+    });
+  });
+    
+  }
+
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+{
+	NSLog(@"scroll end1");
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
+{
+	if (!decelerate) {
+		NSLog(@"scroll end2");
+	}
+}
+
+//サブキューで処理するメソド、子供でオーバーライドする
+-(void)execSubQueue :(NSIndexPath *)indexPath
+{
+sleep(1);
+
+
+
+    NSInteger i = 1;
+    for( i=1; i<=10; i++ ){
+    
+    
+      NSString *str = [NSString stringWithFormat :@"%d", indexPath.row + i];
+      [arr addObject:str];
+      NSIndexPath *index = [NSIndexPath indexPathForRow:(indexPath.row + i) inSection:0];
+      [table insertRowsAtIndexPaths :[NSArray arrayWithObject:index] withRowAnimation:UITableViewRowAnimationFade];
+    }
+}
+
+//サブキューからメインキューに帰ってきたときに処理するメソド、子供でオーバーライドする
+//[super execMainQueue]でサブキューのメモリをリリースし、くるくるを止める
+-(void)execMainQueue
+{
+    [table endUpdates];
+  [loadingIndicator removeFromSuperview];
+  [loadingView removeFromSuperview];
+  dispatch_release(subQueue);
+}
+
+
+
+
+
 
 - (void)viewWillAppear:(BOOL)animated {
   NSIndexPath* selection = [table indexPathForSelectedRow];
@@ -30,9 +103,11 @@
 
 - (void)viewDidAppear:(BOOL)animated {
   [self.table flashScrollIndicators];
+  flg = 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+  NSInteger hoge = arr.count;
      return arr.count;
 }
 
@@ -46,6 +121,7 @@
     }
 
     // Set up the cell...
+    NSLog(@" aaaa %d", indexPath.row);
     cell.text = [arr objectAtIndex :indexPath.row];
     return cell;
 }
@@ -55,6 +131,7 @@
     [arr addObject:@"eee"];
     [table insertRowsAtIndexPaths
       :[NSArray arrayWithObject:index] withRowAnimation:nil];
+      
 }
 
 
@@ -75,18 +152,38 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
     
-    arr = [[NSMutableArray alloc]initWithObjects:
-        @"aaa",
-        @"bbb",
-        @"ccc",
-        @"ddd",
-        nil
-    ];
+    
+    arr = [[NSMutableArray alloc] init];
+    
+    // Do any additional setup after loading the view from its nib.
+    for( int i=0; i<=10; i++ ){
+      NSString *str = [NSString stringWithFormat :@"%d", i];
+      [arr addObject:str];
+    }
+
+/*
+label = [[UILabel alloc] initWithFrame:CGRectMake(100, 300, 60, 30)];
+label.text = @"hohoho";
+[table addSubview:label];
+*/  
     
     table.delegate = self;
     table.dataSource = self;
+  
+  
+  //くるくる回るインジケーターをセット
+  loadingIndicator = [[
+    UIActivityIndicatorView alloc]
+    initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge
+  ];
+  [loadingIndicator setFrame:CGRectMake ((320/2)-20, (480/2)-60, 40, 40)]; 
+  [loadingIndicator startAnimating];
+
+  //半透明のViewをセット
+  loadingView = [
+    [LoadingView alloc] initWithFrame:[[UIScreen mainScreen] bounds]
+  ];
 }
 
 - (void)viewDidUnload
